c2aa6a7d8739c513e14d2f6d034f0d2d21ce429c
Vanja Štefanec <vanja.stefanec@gmail.com>
2013-04-28 20:38:27 +0200

    Modifying models

--- jamtag/jam/models.py
+++ jamtag/jam/models.py
@@ -2,40 +2,67 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=255)
-    url = models.URLField(_(u'url'))
-    songs = models.ManyToManyField('Song', verbose_name=_(u'songs'), through='ArticleSong')
+class Content(models.Model):
+    title = models.CharField(_(u'content title'), max_length=255)
+    tracks = models.ManyToManyField('Track', verbose_name=_(u'track'), through='ContentTrack')
 
     class Meta:
-        verbose_name = _('Article')
-        verbose_name_plural = _('Articles')
+        verbose_name = _('Content')
+        verbose_name_plural = _('Content units')
 
     def __unicode__(self):
         pass
 
 
-class Song(models.Model):
-    title = models.CharField(max_length=255)
-    url = models.URLField(_(u'url'))
+class Track(models.Model):
+    id = models.IntegerField(_(u'Jamendo track ID'), primary_key=True)
+    name = models.CharField(_(u'Jamendo track name'), max_length=255)
+    artist_name = models.CharField(_(u'Jamendo artist name'), max_length=255)
+    audio = models.URLField(_(u'link to track'))
 
     class Meta:
-        verbose_name = _('Song')
-        verbose_name_plural = _('Songs')
+        verbose_name = _('Track')
+        verbose_name_plural = _('Tracks')
 
     def __unicode__(self):
         pass
 
 
-class ArticleSong(models.Model):
-    song = models.ForeignKey('Song')
-    article = models.ForeignKey('Article')
-    times_tagged = models.IntegerField(default=0)
+class ContentTrack(models.Model):
+    track = models.ForeignKey('Track')
+    content = models.ForeignKey('Content')
+    times_tagged = models.IntegerField(default=1)  # postaje redundantno ali mozemo zadrzati radi orderinga
 
     class Meta:
-        verbose_name = _('ArticleSong')
-        verbose_name_plural = _('ArticleSongs')
+        verbose_name = _('Tagged content')
+        verbose_name_plural = _('Tagged content units')
         ordering = ('times_tagged',)
+
+    def __unicode__(self):
+        pass
+
+
+class TagInfo(models.Model):
+    user = models.CharField(max_length=50)  # username Jamendo usera ili neki drugi identifikator
+    tag = models.ForeignKey('ContentTrack')
+    is_tagged = models.BooleanField()
+    is_confirmed = models.BooleanField()
+
+    class Meta:
+        verbose_name = _('Tag information')
+        verbose_name_plural = _('Tag information')
+
+    def __unicode__(self):
+        pass
+
+
+class URL(models.Model):
+    url = models.URLField(_(u'URL'), db_index=True)
+    content = models.ForeignKey('Content')
+
+    class Meta:
+        verbose_name = _('URL')
+        verbose_name_plural = _('URLs')
 
     def __unicode__(self):
         pass
