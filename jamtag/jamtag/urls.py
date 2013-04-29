f9896942e906afdfb53e60f52e590a1fa8b924da
Zlatko <zlatko.masek@gmail.com>
2013-04-29 09:56:49 +0200

    Preparing for api.

--- jamtag/jamtag/urls.py
+++ jamtag/jamtag/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+from tastypie.api import Api
+api = Api(api_name='v1')
+
+from jam.api import ContentResource
+from jam.api import TrackResource
+api.register(ContentResource())
+api.register(TrackResource())
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'jamtag.views.home', name='home'),
     # url(r'^jamtag/', include('jamtag.foo.urls')),
+    url(r'^api/', include(api.urls)),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
