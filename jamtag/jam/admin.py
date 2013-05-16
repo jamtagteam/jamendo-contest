f5c03e5f4a3666f0b5fae02dc6d97bed69ef5c04
chubz <chubzor@gmail.com>
2013-05-16 13:59:37 +0200

    Solved the missing m2m widget with inlines

--- jamtag/jam/admin.py
+++ jamtag/jam/admin.py
@@ -3,8 +3,20 @@ from django.contrib import admin
 from .models import Content, Track, ContentTrack, TagInfo, URL
 
 
-admin.site.register(Content)
-admin.site.register(Track)
-admin.site.register(ContentTrack)
+class ContentTrackInline(admin.StackedInline):
+    model = ContentTrack
+    extra = 1
+
+
+class ContentAdmin(admin.ModelAdmin):
+    inlines = (ContentTrackInline,)
+
+
+class TrackAdmin(admin.ModelAdmin):
+    inlines = (ContentTrackInline,)
+
+
+admin.site.register(Content, ContentAdmin)
+admin.site.register(Track, TrackAdmin)
 admin.site.register(TagInfo)
 admin.site.register(URL)
