75d5d405a8a9b6c85b8a2aa006ff37617d5759e0
chubz <chubzor@gmail.com>
2013-05-21 12:01:00 +0200

    Adding mock frontend for api testing

--- jamtag/jam/views.py
+++ jamtag/jam/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from __future__ import absolute_import
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+index = HomeView.as_view()
--- jamtag/jamtag/urls.py
+++ jamtag/jamtag/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -15,4 +19,6 @@ api.register(ContentResource())
 urlpatterns = patterns('',
     url(r'^api/', include(api.urls)),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'jam.views.index', name='index'),
 )
+urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
