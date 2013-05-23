369bd7518fe9ea6ddeaabe86f2fc154bf36c793d
chubz <chubzor@gmail.com>
2013-05-24 01:16:36 +0200

    Added some preparation for search

--- jamtag/jam/static/js/main.js
+++ jamtag/jam/static/js/main.js
@@ -53,18 +53,41 @@ $(function() {
                         });
                     }
                     else{
-                        self.element.html('whatevs');
+                        var playlist = []
+                        var msg = {
+                            title: 'Please tag this adress!',
+                            mp3: null,
+                        }
+                        playlist.push(msg);
+                        new jPlayerPlaylist({
+                            jPlayer: "#jquery_jplayer_1",
+                            cssSelectorAncestor: "#jp_container_1"
+                        }, playlist, {
+                            swfPath: api.static + "js/lib/jPlayer",
+                            supplied: "mp3",
+                            wmode: "window",
+                            smoothPlayBar: true,
+                            keyEnabled: true
+                        });
                     }
                 }
             );
-            this.on($(document), '#search', 'click', 'searchJamendo');
+            this.on($(document), '.jam-search', 'keyup', 'searchJamendo'); //bilo bi fora da se popunjava bez search btna
             this.on($(document), '#confirm', 'click', 'confirmTag');
             this.on($(document), '#tag-existing', 'click', 'tagExisting');
             this.on($(document), '#tag-new', 'click', 'tagNew');
         },
         searchJamendo: function(el, ev) {
             ev.preventDefault();
-            if ($('#search-input').val()){
+            if ($('.jam-search').val()){
+// ja bi koristio http://developer.jamendo.com/v3.0/autocomplete ovo za querijanje!!!
+                url = 'http://api.jamendo.com/v3.0/autocomplete/'
+                client = 'b6747d04'   // client id for read api testing provided by jamendo
+                format = 'jsonpretty'
+                limit = 3
+                prefix = $('.jam-search').val()
+                matchcount = 1
+
                 // TODO: query jamendo and display results
             }
         },
