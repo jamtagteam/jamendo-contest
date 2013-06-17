a3247ffc5c0968b18a7b47efc1fa2f0985d92c07
Zlatko <zlatko.masek@gmail.com>
2013-06-17 23:09:24 +0200

    Setting up landing page.

--- jamtag/jam/views.py
+++ jamtag/jam/views.py
@@ -1,8 +1,16 @@
 from __future__ import absolute_import
 from django.views.generic import TemplateView
+from .models import Track, URL
 
 
 class HomeView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['pages'] = URL.objects.count()
+        context['tracks'] = Track.objects.count()
+        context['tracks_avg'] = context['tracks'] / context['pages']
+        return context
 
 index = HomeView.as_view()
